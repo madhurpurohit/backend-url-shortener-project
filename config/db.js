fb01349d3ee3a0be3840cad7c65df16d25210079
Madhur Purohit <madhurpurohit1234@gmail.com>
2025-10-25 01:14:47 +0530

    Fix DB Connection Error

--- config/db.js
+++ config/db.js
@@ -4,11 +4,27 @@ import mysql from "mysql2/promise";
 import fs from "fs";
 import path from "path";
 
-const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  ssl: {
-    ca: fs.readFileSync(path.join(process.cwd(), "ca.pem")),
-  },
-});
+const ca_path = process.env.RENDER
+  ? process.env.CA_PEM_PATH
+  : path.join(process.cwd(), "ca.pem");
+
+const connectionString = process.env.DATABASE_URL.split("?")[0];
+
+const poolOptions = {
+  uri: connectionString,
+};
+
+if (ca_path && fs.existsSync(ca_path)) {
+  poolOptions.ssl = {
+    ca: fs.readFileSync(ca_path),
+  };
+  console.log("SSL certificate loaded successfully.");
+} else {
+  console.warn(
+    `CA certificate not found at path: ${ca_path}. SSL will not be used. This will likely cause a connection error if the database requires SSL.`
+  );
+}
+
+const pool = mysql.createPool(poolOptions);
 
 export const db = drizzle(pool);
